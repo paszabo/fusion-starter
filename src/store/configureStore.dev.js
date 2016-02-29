2b9e78f32754ea6384cb86b9e1ca65d07f393560
Cory House <housecor@gmail.com>
2016-02-29 14:36:47 -0600

    Separated dev and prod versions of stores so hot reloading code only exists in dev version.

---
+++ src/store/configureStore.dev.js
@@ -0,0 +1,32 @@
+//This file merely configures the store for hot reloading.
+//This boilerplate file is likely to be the same for each project that uses Redux.
+//With Redux, the actual stores are in /reducers.
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers';
+import thunkMiddleware from 'redux-thunk';
+
+//Without middleware, Redux only supports synchronous data flow. http://redux.js.org/docs/advanced/AsyncFlow.html
+//So we're applying the thunk middleware to support async (ajax calls) within our actions.
+//To clarify, middleware simply lets us slap some behavior in between dispatching an action
+//and the moment it reaches the reducer. You can do other things like log, crash report, route, and so on.
+//Thunk middleware lets us dispatch() functions, (useful for handling ajax calls in reducers)
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+
+export default function configureStore(initialState) {
+  let store;
+  if (window.devToolsExtension) { //Enable Redux devtools if the extension is installed in developer's browser
+    store = window.devToolsExtension()(createStoreWithMiddleware)(rootReducer, initialState);
+  } else {
+    store = createStoreWithMiddleware(rootReducer, initialState);
+  }
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers');
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
+}
