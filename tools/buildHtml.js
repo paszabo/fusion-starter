06515400309f81b121b38a5eb673355f5c98cd69
Cory House <housecor@gmail.com>
2016-03-25 14:41:30 -0500

    moved message to occur at proper time

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -26,7 +26,6 @@ fs.readFile('src/index.html', 'utf8', function (err,data) {
 
   fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
     if (err) return console.log(err);
+    console.log('index.html written to /dist'.green);
   });
-
-  console.log('index.html written to /dist'.green);
 });
