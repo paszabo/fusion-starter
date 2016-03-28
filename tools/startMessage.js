7d293c636ef130642af632f89d5ca99fd9d65085
Cory House <housecor@gmail.com>
2016-03-28 15:27:21 -0500

    Added start message

---
+++ tools/startMessage.js
@@ -0,0 +1,6 @@
+import colors from 'colors';
+
+/*eslint-disable no-console */
+
+console.log('Starting app in development mode, running tests, linting, and opening default browser at http://localhost:3000...'.green);
+
