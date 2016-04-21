ae050879305c786fbfc19325eff968e644969281
Cory House <cory.house@vinsolutions.com>
2016-04-21 14:58:46 -0500

    Adding Bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import configureStore from './store/configureStore';
-import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+// Yep, that's right. You can import SASS/CSS files too!
+// Webpack will run the associated loader and plug this into the page.
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.scss';
+
 const store = configureStore();
 
 render(
