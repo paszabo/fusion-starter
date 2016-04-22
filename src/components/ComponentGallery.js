8930ddafbe959f19dd9780265b205eb018655aa6
Cory House <housecor@gmail.com>
2016-04-22 15:22:50 -0500

    Added react-bootstrap and draft.js examples

--- src/components/ComponentGallery.js
+++ src/components/ComponentGallery.js
@@ -1,37 +1,20 @@
 import React, {PropTypes} from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
-import Modal from 'react-bootstrap/lib/Modal';
-import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import Popover from 'react-bootstrap/lib/Popover';
 import Tooltip from 'react-bootstrap/lib/Tooltip';
+import BootstrapModal from './gallery/BootstrapModalExample';
+import DraftJs from './gallery/DraftJsExample';
 
 class ComponentGallery extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.state = { showModal: false };
-    this.close = this.close.bind(this);
-    this.open = this.open.bind(this);
-  }
-
-  close() {
-    this.setState({ showModal: false });
-  }
-
-  open() {
-    debugger;
-    this.setState({ showModal: true });
-  }
-
   render() {
-    let popover = <Popover id="my-popover" title="popover">very popover. such engagement</Popover>;
-    let tooltip = <Tooltip id="my-tooltip">wow.</Tooltip>;
-
     return (
       <div>
         <h1>Component Gallery</h1>
         <p>Recommended components:</p>
+
+        <h2><a href="https://facebook.github.io/draft-js/">Draft.js</a></h2>
+        <DraftJs/>
 
         <h2><a href="https://react-bootstrap.github.io">React Bootstrap</a></h2>
         <h3>Button</h3>
@@ -45,48 +28,7 @@ class ComponentGallery extends React.Component {
         </ButtonGroup>
 
         <h3>Modal</h3>
-        <div>
-
-          <Button
-            bsStyle="primary"
-            bsSize="large"
-            onClick={this.open}
-          >
-            Launch demo modal
-          </Button>
-
-          <Modal show={this.state.showModal} onHide={this.close}>
-            <Modal.Header closeButton>
-              <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <h4>Text in a modal</h4>
-              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-
-              <h4>Popover in a modal</h4>
-              <p>there is a <OverlayTrigger overlay={popover}><a href="#">popover</a></OverlayTrigger> here</p>
-
-              <h4>Tooltips in a modal</h4>
-              <p>there is a <OverlayTrigger overlay={tooltip}><a href="#">tooltip</a></OverlayTrigger> here</p>
-
-              <hr />
-
-              <h4>Overflowing text to show scroll behavior</h4>
-              <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-              <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-              <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-              <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-              <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-              <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={this.close}>Close</Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
+        <BootstrapModal/>
       </div>
     );
   }
