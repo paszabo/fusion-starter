3164f64bdfa1c895b2f005951231f9439f9990fd
Cory House <housecor@gmail.com>
2016-05-11 11:49:38 -0500

    Linting fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import routes from './routes';
 import {Router, browserHistory} from 'react-router';
-import configureStore from './store/configureStore';
+import configureStore from './store/configureStore'; // eslint-disable-line import/default
 // Yep, that's right. You can import SASS/CSS files too!
 // Webpack will run the associated loader and plug this into the page.
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -12,7 +12,7 @@ import './styles/styles.scss';
 const store = configureStore();
 
 // Don't need React Router?
-// Reference your top level component instead of <Router> below 
+// Reference your top level component instead of <Router> below
 render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
