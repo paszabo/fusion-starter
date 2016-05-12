a0591ea6f37f25e4390168b2f414ea717dbe1b63
Cory House <housecor@gmail.com>
2016-05-12 14:09:49 -0500

    Added images of upcoming pages.

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,16 @@ import App from './components/App';
 import FuelSavingsPage from './components/fuelSavings/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
 import CustomersPage from './components/customers/CustomersPage';
 import ComponentGallery from './components/gallery/ComponentGallery';
+import StyleGuide from './components/styleGuide/styleGuide';
+import Dashboard from './components/dashboard/dashboard';
 import HomePage from './components/HomePage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="gallery" component={ComponentGallery}/>
+    <Route path="style-guide" component={StyleGuide}/>
+    <Route path="dashboard" component={Dashboard}/>
     <Route path="fuelsavings" component={FuelSavingsPage}/>
     <Route path="customers" component={CustomersPage}/>
   </Route>
