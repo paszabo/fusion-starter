66ab4304f4c4754a4584adf4f95b9bc9e16db20c
Cory House <housecor@gmail.com>
2016-07-08 09:14:30 -0500

    Fixed check for minimum node version

--- tools/nodeVersionCheck.js
+++ tools/nodeVersionCheck.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
 var exec = require('child_process').exec;
-exec('npm -v', function (err, stdout, stderr) {
+exec('node -v', function (err, stdout, stderr) {
   if (err) throw err;
-  if (parseFloat(stdout) < 4) {
-    throw new Error('ERROR: Fusion Starter requires npm version 4.0 or greater.');
+  if (parseFloat(stdout) < 5) {
+    throw new Error('ERROR: Fusion Starter requires Node version 5.0 or greater.');
     process.exit(1);
   }
 });
