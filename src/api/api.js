2d97cc09c2acaf18be800c8ae080cb4c50ba29f2
Cory House <housecor@gmail.com>
2016-07-22 13:35:06 -0500

    Centralize baseUrl configuration so it can be referenced from multiple places.

--- src/api/api.js
+++ src/api/api.js
@@ -3,15 +3,9 @@
 // If you are already using jQuery, you can use it to make AJAX calls here instead since
 // Axios weighs about 20k. But note that jQuery's promise implementation is quirky.
 import axios from 'axios';
+import getBaseUrl from '../utils/baseUrl';
 
 const api = null;
-
-const getBaseUrl = function() {
-  const inDevelopment = window.location.hostname === 'localhost';
-  // Note that this baseUrl assumes you're running CarDashboard via IIS at vinconnect.com via a hosts file entry.
-  // See https://github.com/cox-auto-kc/fusion-starter#initial-machine-setup for more info.
-  return inDevelopment ? 'http://vinconnect.com/CarDashboard/' : '/CarDashboard/';
-};
 
 // Note that this can't be called until we're in the browser
 // since we need to sniff the URL to know what environment we're in
