0f592ae0d7b1309e43dcb8208f424c16d42abf19
Cory House <housecor@gmail.com>
2016-08-05 14:32:32 -0500

    Exclude node_modules instead of including /src. This supports transpiling code outside of /src if necessary. See here for more info: https://github.com/coryhouse/react-slingshot/issues/224#event-747044481.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,5 @@
 // For info about this file refer to webpack and webpack-hot-middleware documentation
 import webpack from 'webpack';
-import path from 'path';
 import NpmInstallPlugin from 'npm-install-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import StyleLintPlugin from 'stylelint-webpack-plugin';
@@ -44,7 +43,7 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /(\.js|\.jsx)$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /(\.js|\.jsx)$/, exclude:/node_modules/, loader: 'babel'},
       {test: /\.css$/, loader: 'style-loader?sourceMap!css-loader?sourceMap!postcss-loader'},
       {test: /\.ico(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?mimetype=image/x-icon'},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,6 @@
 // For info about this file refer to webpack and webpack-hot-middleware documentation
 // For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
 import webpack from 'webpack';
-import path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
@@ -76,7 +75,7 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /(\.js|\.jsx)$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /(\.js|\.jsx)$/, exclude:/node_modules/, loader: 'babel'},
       {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader?sourceMap', 'css-loader?sourceMap!postcss-loader')},
       {test: /\.ico(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?mimetype=image/x-icon'},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
