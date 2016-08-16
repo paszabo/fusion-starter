810df8fa8ad009e6d354845c47486b70d4ee8c43
Nathan Trost <Nathaniel.Trost@vinsolutions.com>
2016-08-16 13:01:59 -0500

    Fix for Navigation, allow for scrolling on vertical minimization #83  PBI 53052

--- src/components/pages/DataTablesPage.js
+++ src/components/pages/DataTablesPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Griddle from 'griddle-react';
 import {PageHeader, Panel, Well, Button} from 'react-bootstrap';
+import GriddleCustomPager from '../widgets/Griddle/GriddleCustomPager';
 
 const vehicleData = require('../../data/vehicles');
 
@@ -11,20 +12,32 @@ const DataTablesPage = () => {
 
       <Panel header='Advanced Data Tables'>
 
-        <Griddle results={vehicleData} tableClassName='table' showFilter={true}
-          showSettings={true} initialSort='Make' initialSortAscending={true}
+        <Griddle
+          tableClassName='table'
           useGriddleStyles={false}
-          columns={['Make', 'Model', 'Year', 'Price']} />
+          initialSort='Make'
+          initialSortAscending={true}
+
+
+          results={vehicleData}
+          columns={['Make', 'Model', 'Year', 'Price']}
+
+          showFilter={true}
+          showSettings={true}
+
+          useCustomPagerComponent={true}
+          customPagerComponent={GriddleCustomPager}
+        />
         <br/>
 
         <Well style={{marginBottom: 0}}>
           <h4>Griddle Usage Information</h4>
 
           <p>Griddle is a very flexible, advanced grid component for React. In Fusion UI, we are using a customized version of Griddle styled for Bootstrap 3.
-          We have also customized many user ineterface elements to use React-Bootstrap components. For complete documentation on Griddle, visit the Griddle website
-          at <Button bsStyle='link' style={{padding: 0}} target='_blank' href='http://griddlegriddle.github.io/Griddle/'>{'http://griddlegriddle.github.io/Griddle/'}</Button>.</p>
+            We have also customized many user ineterface elements to use React-Bootstrap components. For complete documentation on Griddle, visit the Griddle website
+            at <Button bsStyle='link' style={{padding: 0}} href='http://griddlegriddle.github.io/Griddle/'>{'http://griddlegriddle.github.io/Griddle/'}</Button>.</p>
 
-          <Button bsSize='large' block href='http://griddlegriddle.github.io/Griddle/' target='_blank'>View Griddle Documentation</Button>
+          <Button bsSize='large' block href='http://griddlegriddle.github.io/Griddle/'>View Griddle Documentation</Button>
         </Well>
 
       </Panel>
--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,7 @@ import DashboardLayout from './components/layouts/DashboardLayout';
 
 import HomePage from './components/pages/HomePage';
 import SavingsCalculatorPage from './components/pages/SavingsCalculatorPage';
-//import DataTablesPage from './components/pages/DataTablesPage';
-import DataTablesPage from './components/pages/DataTablesPageWithPagination';
+import DataTablesPage from './components/pages/DataTablesPage';
 import ChartsPage from './components/pages/ChartsPage';
 import RichTextEditorPage from './components/pages/RichTextEditorPage';
 import LazyLoadingPage from './components/pages/LazyLoadingPage';
