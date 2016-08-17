8d0ad450a05ced5e06e057defa600f18eef7b859
AUTOTRADER\dpamidi <divya.pamidi@vinsolutions.com>
2016-08-17 08:36:42 -0500

    Adding RichText Editor

--- src/components/pages/RichTextEditorPage.js
+++ src/components/pages/RichTextEditorPage.js
@@ -1,17 +1,7 @@
 /* eslint-disable react/no-multi-comp */
 import React from 'react';
 import {PageHeader, Panel, Well, Button} from 'react-bootstrap';
-
-// TODO: Replace with actual implementation and remove iframe...
-const RTEFrame = () => (
-  <div>
-    <iframe
-      style={{border: 'none'}}
-      width="600" height="390"
-      src={`https://coxautokc.github.io/react-richtext/`}
-    />
-  </div>
-);
+import RichEditorDemo from '../widgets/RichTextEditor/RichEditorDemo';
 
 const RichTextEditorPage = () => {
   return (
@@ -19,17 +9,17 @@ const RichTextEditorPage = () => {
       <PageHeader>Rich Text Editor <small>Draft.js</small></PageHeader>
 
       <Panel>
-        <center><RTEFrame /></center>
-
+        <RichEditorDemo />
         <Well>
-          <h4>Draft.js Usage Information</h4>
+
+          <h4>Drart.js Usage Information</h4>
 
           <p>Draft.js is a framework for building rich text editors in React, powered by an immutable model and abstracting over cross-browser differences.
-          Draft.js makes it easy to build any type of rich text input, whether you are just looking to support a few inline text styles or building a complex
-          text editor for composing long-form articles. For complete documentation on Draft.js, visit the Facebook{"'"}s Draft.js website
-          at <Button bsStyle='link' style={{padding: 0}} target='_blank' href='https://facebook.github.io/draft-js/'>{'https://facebook.github.io/draft-js/'}</Button>.</p>
+            Draft.js makes it easy to build any type of rich text input, whether you are just looking to support a few inline text styles or building a complex
+            text editor for composing long-form articles. For complete documentation on Draft.js, visit the Facebook{"'"}s Draft.js website
+            at <Button bsStyle='link' style={{padding: 0}} href='https://facebook.github.io/draft-js/'>{'https://facebook.github.io/draft-js/'}</Button>.</p>
 
-          <Button bsSize='large' target='_blank' block href='https://facebook.github.io/draft-js/'>View Draft.js Documentation</Button>
+          <Button bsSize='large' block href='https://facebook.github.io/draft-js/'>View Draft.js Documentation</Button>
         </Well>
       </Panel>
 
